730a7a92a57505abce59ea6a9c5c882ea6e52bb5
greensea <gs@bbxy.net>
2015-04-23 11:38:51 +0800

    Save downloaded videos to videos/ directory. 将下载的视频保存到 videos 目录下

--- videos/get.sh
+++ videos/get.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 URL_PREFIX="http://youxiamotors.com/upload/lane-detection"
+DIR=$(dirname $0)
 
-wget -c $URL_PREFIX/13510002.MOV
-wget -c $URL_PREFIX/16290029.MOV
-wget -c $URL_PREFIX/17050041.MOV
+for file in 13510002.MOV 16290029.MOV 17050041.MOV
+do
+	wget -c $URL_PREFIX/$file -O $DIR/$file
+done
 
 exit 0
